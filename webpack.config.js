c580de2d642feac41b7cd2c0ad6475d705d3f6e6
AkshayKhot07 <akshaykhot743@gmail.com>
2022-06-08 20:29:54 +0530

    refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,4 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "./dist"),
   },
-  module: {
-    rules: [
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
 };
