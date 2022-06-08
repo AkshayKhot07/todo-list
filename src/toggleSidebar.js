bee2969a6dcc11549255f993c72352473233304f
AkshayKhot07 <akshaykhot743@gmail.com>
2022-06-08 16:18:50 +0530

    Mobile responsiveness implemented

--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -1,11 +1,40 @@
 export const toggleSidebar = () => {
-  const menuIcon = document.querySelector(".menu-icon");
+  let inputToggler = document.querySelector("#toggle");
   const aside = document.querySelector("aside");
   const main = document.querySelector("main");
-  const container = document.querySelector(".container");
-  const sometest = document.querySelector(".default-task-list");
+  const defaultTaskList = document.querySelector(".default-task-list");
+  const projectTaskList = document.querySelector(".projects-task-list");
 
-  menuIcon.addEventListener("click", () => {
-    aside.classList.toggle("hideSidebar");
+  // inputToggler.checked = false;
+  inputToggler.addEventListener("click", () => {
+    if (inputToggler.type == "checkbox" && inputToggler.checked == true) {
+      console.log("input checked");
+      aside.style.left = "0";
+      main.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+
+      //sidebar closes on click of input, today and this week button
+      defaultTaskList.querySelectorAll("button").forEach((button) => {
+        button.addEventListener("click", () => {
+          inputToggler.checked = false;
+          aside.style.left = "-70%";
+          main.style.backgroundColor = "#eee";
+        });
+      });
+
+      //sidebar closes on click of any respective project div on aside
+      projectTaskList.querySelectorAll("div").forEach((div) => {
+        div.addEventListener("click", () => {
+          inputToggler.checked = false;
+          aside.style.left = "-70%";
+          main.style.backgroundColor = "#eee";
+        });
+      });
+    }
+
+    if (inputToggler.type == "checkbox" && inputToggler.checked == false) {
+      console.log("input not checked");
+      aside.style.left = "-70%";
+      main.style.backgroundColor = "#eee";
+    }
   });
 };
